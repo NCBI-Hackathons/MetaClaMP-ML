bb5f44677b53cd459fc0f232779999c80d31f645
kumardeep27 <ckumardeep@gmail.com>
2019-08-14 15:58:57 -0400

    added rf_sklearn_loo code

--- rf_sklearn_LOO.py
+++ rf_sklearn_LOO.py
@@ -1,24 +1,24 @@
 import pandas as pd
-import numpy as np
-from sklearn.svm import LinearSVC
 from sklearn.model_selection import LeaveOneOut
+from sklearn.svm import LinearSVC
+import numpy as np
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import classification_report
 from sklearn.metrics import roc_auc_score
 from sklearn.feature_selection import SelectFromModel
-
-humann2=pd.read_csv("humann2_output",index_col=0)
-X = humann2[humann2.columns[13:]]
+from sklearn.ensemble import ExtraTreesClassifier
+humann2=pd.read_csv("sample11.csv",index_col=0)
+X = humann2[humann2.columns[2:]]
 y=humann2['flag']
 yy=y
-
 #binarize the labels
 from sklearn.preprocessing import Binarizer
 ############Feature selection ############################3
-lsvc = LinearSVC(C=0.1, penalty="l1", dual=False).fit(X, yy.ravel())
-model = SelectFromModel(lsvc, prefit=True)
+rf_clf = RandomForestClassifier(n_estimators=100)
+rf_clf = rf_clf.fit(X, y)
+model = SelectFromModel(rf_clf, prefit=True)
 X_new = model.transform(X)
-X_new.shape
+print(X_new.shape)
 idxs_selected = model.get_support(indices=True)
 f = X.columns[idxs_selected]
 features_dataframe_new=X[f]
@@ -34,19 +34,22 @@ for train_index, test_index in loo.split(features_dataframe_new):
 	print("TRAIN:", train_index, "TEST:", test_index)
 	X_train, X_test = features_dataframe_new.iloc[train_index], features_dataframe_new.iloc[test_index]
 	y_train, y_test = yy[train_index], yy[test_index]
+	#print(X_train, X_test, y_train, y_test)
+	#clf=SVC(kernel='linear',probability=True,class_weight="balanced")
 	clf = RandomForestClassifier(n_estimators=100, max_depth=2,random_state=0)
 	clf.fit(X_train, y_train.ravel())
 	y_pred11 = clf.predict(X_test) 
 	y_p_d=y_pred11.tolist()
 	final_pred_discrete.extend(y_p_d)
-	y_p_score=clf.predict_proba(X_test)
-	y_t=y_test.tolist() 
+	y_p_score=clf.predict_proba(X_test) ##### getting prob scores (coressponding to SVM score)
+	y_t=y_test.tolist() # converting np array to list
 	y_predicted=y_p_score.tolist()
-	y_p_s1=[i[1] for i in y_predicted]
+	y_p_s1=[i[1] for i in y_predicted] # feteching 2nd element of 2 d array
 	final_pred.extend(y_p_s1)
 	final_actual.extend(y_t)
-res = "\n".join("{}\t{}".format(x, y) for x, y in zip(final_actual, final_pred))
+res = "\n".join("{} {}".format(x, y) for x, y in zip(final_actual, final_pred))
 print(res,file=f)
 print(classification_report(final_actual, final_pred_discrete))
 print("AUROC")
 print(roc_auc_score(final_actual, final_pred))
+
