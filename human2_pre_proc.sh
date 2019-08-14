9908d7a2bfd387c5f192052cffb25f78686805c3
Anna <anna.k.belford@gmail.com>
2019-08-14 12:42:15 -0400

    Add files via upload

---
+++ human2_pre_proc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+#create temp submit file 1
+ls *.fasta > temp_submission
+
+#create temp submit file2
+awk '{print "humann2 --input " $0 " --output humann_out_"$0}' temp_submission > temp_submission_2 #humann2_submission.swarm
+
+#clean for final swarm submit file
+sed 's/......$//' temp_submission_2 > humann2_submission.swarm
+
+#remove temp files
+rm -r temp_submission
+
+rm -r temp_submission_2
