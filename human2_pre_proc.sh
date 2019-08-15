137af51b49f93c39f18598f2e2e4725c7bb8a41e
Anna <anna.k.belford@gmail.com>
2019-08-15 09:10:22 -0400

    Update human2_pre_proc.sh

--- human2_pre_proc.sh
+++ human2_pre_proc.sh
@@ -5,7 +5,7 @@
 ls *.fasta > temp_submission
 
 #create temp submit file2
-awk '{print "humann2 --input " $0 "--threads 16 --output humann_out_"$0}' temp_submission > temp_submission_2 #humann2_submission.swarm
+awk '{print "humann2 --input " $0 "--threads 56 --output humann_out_"$0}' temp_submission > temp_submission_2 #humann2_submission.swarm
 
 #clean for final swarm submit file
 sed 's/......$//' temp_submission_2 > humann2_submission.swarm
